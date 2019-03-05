84a10711072b963466cb6cbada80631c5644dfa8
hp <namstudionsk@gmail.com>
2019-03-05 23:37:00 +0700

    removed files from cache

--- srv42/srv42/srv42-portlet/target/srv42-portlet-1.0-SNAPSHOT/WEB-INF/sql/indexes.sql
+++
@@ -1,4 +0,0 @@
-create index IX_B8D2E7B3 on srvv_Record (groupId);
-create index IX_12D4CAFD on srvv_Record (uuid_);
-create index IX_4533690B on srvv_Record (uuid_, companyId);
-create unique index IX_DE08B64D on srvv_Record (uuid_, groupId);
